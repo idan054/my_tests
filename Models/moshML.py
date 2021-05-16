5bc53770bd3cd50f4a0f344ef86145ab0f86b5bf
Biton's Mac <idanbit80@gmail.com>
2021-05-16 17:19:09 +0300

    V1.0 Models/moshML.py - working with ML | Focus: Measure accuracy - Measure accuracy - A working prediction model

--- Models/moshML.py
+++ Models/moshML.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from color_printer import *
 
 ## Machine learning A to predict your music genre
 #* Based your gender and age (and data ofcourse)
@@ -7,6 +8,7 @@ df_musicFile = pd.read_csv("../Resources/music.csv")
 # print(df_musicFile.shape) # overall (records, columns)
 # print(df_musicFile.describe()) # basic info (mean - average values)
 # print(df_musicFile.values) # list of rows (every row is also a list)
+printYellow("Overall .csv data:")
 print(df_musicFile) # Print the csv
 
 # region the music.csv
@@ -41,16 +43,38 @@ x_inputValues = df_musicFile.drop (columns=['genre'])
 y_outputValues = df_musicFile["genre"]
 # print(y_outputValues)
 
+from sklearn.model_selection import train_test_split
+x_input_train, x_input_test,\
+y_output_train, y_output_test = \
+    train_test_split(x_inputValues, y_outputValues,
+                 test_size=0.3) # 30% result data save for testing
+
 ## Part 2 - Train the model and .predict
 from sklearn.tree import DecisionTreeClassifier
 model = DecisionTreeClassifier()
 
 # train the model by insert the input (x) and output (y)
-model.fit(X=x_inputValues, y=y_outputValues)
+model.fit(X=x_input_train, y=y_output_train)
 
 # Ask the mode to make a 2 predictions
 # by new insert new x_inputValues records
-predictions = model.predict([[22, 1], [22, 0]]) # 22yrs old Man & Woman
-print(predictions)
+# printYellow("Testing values .csv x_input_test:")
+# print(x_input_test)
+predictions = model.predict(x_input_test) # 22yrs old Man & Woman
+# predictions = model.predict([[22, 1], [22, 0]]) # 22yrs old Man & Woman
+# print(predictions)
 # >> ['HipHop' 'Dance']
 #~ Great! the model made a predictions based data
+
+## Part 3 - Measure accuracy
+# pre-made: 20% of the csv data has been kept for testing (x_input_test, y_output_test)
+from sklearn.metrics import accuracy_score
+printYellow("Correct .csv y_output_test:")
+print(y_output_test)
+printYellow("predictions:")
+print(*predictions, sep='\n')
+
+# Check accuracy ratio: Correct output VS. predictions output
+accuracy_score = accuracy_score(y_output_test, predictions)
+print(accuracy_score*100, "%")
+
