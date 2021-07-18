3f3dba418a4afb68e2223fe147aa7c414456079d
idan054 <idanbit80@gmai.com>
2021-07-18 20:15:00 +0300

    tests.py Great json edit for spider3d app for control panel: https://config-fluxstore-anfm3v1ro-idan054.vercel.app/

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ from pprint import pprint
 
 from sqlalchemy import true, false, null
 
-resp = {
+resp_form = {
   'post_type': [
     'product',
     'category',
@@ -31,9 +31,9 @@ items_dict = {}
 forIndex = 0
 ## 1 Setup inner dict to place "items" list
 # EXAMPLE: {'1': {'items': []}, '2': {'items': []}}
-for item in resp["slider_num"]: # post_type represent the quantity of rows on control panel
+for item in resp_form["slider_num"]: # post_type represent the quantity of rows on control panel
   items_dict.update({
-  resp["slider_num"][forIndex]: {"items": []},
+  resp_form["slider_num"][forIndex]: {"items": []},
   })
   forIndex += 1
 
@@ -42,11 +42,11 @@ for item in resp["slider_num"]: # post_type represent the quantity of rows on co
 ## 2 place "items" on the right slider
 # EXAMPLE: {'1': {'items': [{'category': '12111', 'image': 'www12', 'padding': 15.0, 'my_slider_num': '1'}]}, '2': {'items': [{'category': '23451', 'image': 'www2', 'padding': 15.0, 'my_slider_num': '2'}]}}
 forIndex = 0
-for item in resp["slider_num"]: # post_type represent the quantity of rows on control panel
-  items_dict[ resp["slider_num"][forIndex] ] \
+for item in resp_form["slider_num"]: # post_type represent the quantity of rows on control panel
+  items_dict[ resp_form["slider_num"][forIndex]] \
   ["items"].append({
-    resp["post_type"][forIndex]: resp["post_num"][forIndex],
-    "image": resp["pic_link"][forIndex],
+    resp_form["post_type"][forIndex]: resp_form["post_num"][forIndex],
+    "image": resp_form["pic_link"][forIndex],
     "padding": 15.0,
     # "my_slider_num": resp["slider_num"][forIndex],
   })
@@ -55,145 +55,14 @@ for item in resp["slider_num"]: # post_type represent the quantity of rows on co
 
 # print(items_dict)
 
-# ATTENTION! true & false should end up WITHOUT any (")
-full_config = {
-  "HorizonLayout": [
-    {
-      "layout": "logo",
-      "showMenu": true,
-      "showSearch": true,
-      "showLogo": true,
-      "menuIcon": {
-        "name": "line_horizontal_3_decrease",
-        "fontFamily": "CupertinoIcons"
-      },
-      "image": "https://i.imgur.com/LQWSjzt.png"
-    },
-    {
-      "layout": "bannerImage",
-      "isSlider": true,
-      "autoPlay": true,
-      "showNumber": false,
-      "design": "default",
-      "showBackGround": true,
-      "radius": 8,
-      "items": [
-        {
-          "product": 25056,
-          "image": "https://i.imgur.com/84ySdG3.jpg",
-          "padding": 15.0
-        }
-      ]
-    },
-    {
-      "layout": "bannerImage",
-      "isSlider": true,
-      "autoPlay": true,
-      "showNumber": false,
-      "design": "default",
-      "showBackGround": true,
-      "radius": 8,
-      "items": [
-        {
-          "category": 4783,
-          "image": "https://i.imgur.com/H0bcI9u.jpg",
-          "padding": 15.0
-        },
-        {
-          "category": 2352,
-          "image": "https://i.imgur.com/H7BJGsh.jpg",
-          "padding": 15.0
-        }
-      ]
-    },
-    {
-      "name": "מדפסות מעולות",
-      "category": 2341,
-      "layout": "threeColumn",
-      "isSnapping": true
-    }
-  ],
-  "Setting": {
-    "MainColor": "#961b1e",
-    "ProductListLayout": "list",
-    "StickyHeader": true,
-    "ProductDetail": "halfSizeImageType",
-    "ShowChat": true,
-    "FontFamily": "Heebo"
-  },
-  "TabBar": [
-    {
-      "layout": "home",
-      "icon": "assets/icons/tabs/icon-home.png",
-      "pos": 100,
-      "key": "j3yj4st8gt"
-    },
-    {
-      "layout": "search",
-      "icon": "assets/icons/tabs/icon-search.png",
-      "pos": 200,
-      "key": "bzl41d82nk"
-    },
-    {
-      "layout": "cart",
-      "icon": "assets/icons/tabs/icon-cart2.png",
-      "pos": 300,
-      "key": "vxll1hkaq6"
-    },
-    {
-      "layout": "profile",
-      "icon": "assets/icons/tabs/icon-user.png",
-      "showChat": true,
-      "pos": 400,
-      "settings": [
-        "order",
-        "wishlist",
-        "darkTheme",
-        "rating",
-        "about"
-      ],
-      "background": "https://i.imgur.com/u2GRG8B.jpeg",
-      "key": "mt0v8wv5c5"
-    },
-    {
-      "layout": "wishlist",
-      "icon": "assets/icons/tabs/icon-heartthin.png",
-      "fontFamily": null,
-      "showChat": false,
-      "pos": 550
-    }
-  ],
-  "Drawer": {
-    "logo": null,
-    "items": [
-      {
-        "type": "home",
-        "show": false
-      },
-      {
-        "type": "blog",
-        "show": true
-      },
-      {
-        "type": "login",
-        "show": true
-      },
-      {
-        "show": true,
-        "type": "category"
-      }
-    ]
-  },
-  "VerticalLayout": {
-    "key": "l7x7ddwj5q",
-    "layout": "menu",
-    "name": "המומלצים שלנו",
-    "isVertical": true
-  },
-  "Version": 1,
-  "Datetime": "15:24:04 - Mon 12 Jul"
-}
+import urllib.request, json
+with urllib.request.urlopen("https://config-fluxstore-47g7m0odz-idan054.vercel.app/") as url:
+  try:
+    full_config = json.loads(url.read().decode())
+  except:
+    full_config = url.read()
 
+## place final "items" of full config
 forIndex = 0
 for item in full_config["HorizonLayout"]:
   # print(item)
@@ -205,7 +74,9 @@ for item in full_config["HorizonLayout"]:
       items_dict[f"{forIndex}"]["items"]
     except: pass
   forIndex += 1
-print(full_config["HorizonLayout"])
+# print(full_config["HorizonLayout"])
+full_config = json.dumps(full_config, indent=1, ensure_ascii=False)
+print(full_config)
 
 sample =      """{
       "layout": "bannerImage",
